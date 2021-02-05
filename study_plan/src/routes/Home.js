357fea3539183947494f71991fb3233edd1d2a89
Choieeh <chyh0182@naver.com>
2021-02-05 14:15:43 +0000

    make Home

---
+++ study_plan/src/routes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component{
+	state = {};
+
+	render(){
+		return(
+		<div>
+	 		<h1>Welcome to study planner</h1>
+		</div>
+		)
+	}
+}
+
+export default Home;
